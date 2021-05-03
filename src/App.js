db0f0499d219078f016359b573ed3c94ea920fa3
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-05-03 19:50:45 +0430

    final

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-
-import React , {useState, useEffect} from 'react';
-import api from './api';
-import AppContext from './AppContext';
-import Child from './Child';
+import React from 'react';
+import Infinitescroll from './Infinitescroll';
 
 
-
-function App() {
-const	[data , setData] = useState([])
-const 	[loading , setloading]= useState(false)
-	const getData = async ()=>{
-		setloading(true);
-		const asyncData = await api();
-		
-		setData(asyncData.data.splice(0,5));
-		setloading(false)
-	}
-	useEffect(()=>{
-		getData();
-	},[])
-	
-  return (
-   <AppContext.Provider value={{loading, data}}>
-	   <Child/>
-
-   </AppContext.Provider>
-  );
+const App = () => {
+	return (<Infinitescroll /> );
 }
-
+ 
 export default App;
--- src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-const api =()=>{
-	return axios.get('https://jsonplaceholder.typicode.com/posts')
-}
-export default api ; 
